c91d43e98b232d0ba8a3bed509d872a82288c0b8
Onur Tascioglu <onurtsc@outlook.com>
2021-04-22 17:50:57 +0300

    read image and weights

--- EmotionClassification/MyForm.cpp
+++ EmotionClassification/MyForm.cpp
@@ -3,7 +3,7 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
-// sea waysss
+
 
 
 [STAThread]
--- EmotionClassification/MyForm.h
+++ EmotionClassification/MyForm.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <windows.h>
+#include "image.h"
+#include "atlstr.h"
+#include <iostream>
+#include <fstream>
+#include <msclr\marshal.h>
+#include <vector>
+#include <istream>
+#include <string>
+#include <sstream>
+
+
 
 namespace EmotionClassification {
 
@@ -8,13 +20,24 @@ namespace EmotionClassification {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
+	using namespace msclr::interop;
+	using namespace System::Runtime::InteropServices;
+	using namespace std;
 
 	/// <summary>
 	/// Summary for MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
+		String^ readFile;
+		BYTE* buffer;
+	private: System::Windows::Forms::ToolStripMenuItem^ fer2013DSToolStripMenuItem;
+
+		   BYTE* raw_intensity;
+		
 	public:
+
 		MyForm(void)
 		{
 			InitializeComponent();
@@ -34,6 +57,15 @@ namespace EmotionClassification {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ dosyaToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ openToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ weightsToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ pictureToolStripMenuItem;
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 
 	private:
 		/// <summary>
@@ -48,12 +80,231 @@ namespace EmotionClassification {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->dosyaToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->openToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->weightsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->pictureToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->fer2013DSToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->menuStrip1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->dosyaToolStripMenuItem });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1451, 28);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// dosyaToolStripMenuItem
+			// 
+			this->dosyaToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->openToolStripMenuItem });
+			this->dosyaToolStripMenuItem->Name = L"dosyaToolStripMenuItem";
+			this->dosyaToolStripMenuItem->Size = System::Drawing::Size(46, 24);
+			this->dosyaToolStripMenuItem->Text = L"File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this->openToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->weightsToolStripMenuItem,
+					this->pictureToolStripMenuItem, this->fer2013DSToolStripMenuItem
+			});
+			this->openToolStripMenuItem->Name = L"openToolStripMenuItem";
+			this->openToolStripMenuItem->Size = System::Drawing::Size(128, 26);
+			this->openToolStripMenuItem->Text = L"Open";
+			// 
+			// weightsToolStripMenuItem
+			// 
+			this->weightsToolStripMenuItem->Name = L"weightsToolStripMenuItem";
+			this->weightsToolStripMenuItem->Size = System::Drawing::Size(167, 26);
+			this->weightsToolStripMenuItem->Text = L"Weights";
+			this->weightsToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::weightsToolStripMenuItem_Click);
+			// 
+			// pictureToolStripMenuItem
+			// 
+			this->pictureToolStripMenuItem->Name = L"pictureToolStripMenuItem";
+			this->pictureToolStripMenuItem->Size = System::Drawing::Size(167, 26);
+			this->pictureToolStripMenuItem->Text = L"Picture";
+			this->pictureToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::pictureToolStripMenuItem_Click);
+			// 
+			// fer2013DSToolStripMenuItem
+			// 
+			this->fer2013DSToolStripMenuItem->Name = L"fer2013DSToolStripMenuItem";
+			this->fer2013DSToolStripMenuItem->Size = System::Drawing::Size(167, 26);
+			this->fer2013DSToolStripMenuItem->Text = L"Fer2013 DS";
+			this->fer2013DSToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::fer2013DSToolStripMenuItem_Click);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(410, 12);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(1029, 513);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(13, 32);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(391, 373);
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1451, 573);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+
+	private: System::Void weightsToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		Stream^ mystream;
+		OpenFileDialog^ openFileDialog1 = gcnew OpenFileDialog;
+
+		openFileDialog1->InitialDirectory = "";
+		openFileDialog1->Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+		openFileDialog1->FilterIndex = 2;
+		openFileDialog1->RestoreDirectory = true;
+
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		{
+			if ((mystream = openFileDialog1->OpenFile()) != nullptr)
+			{
+				// Insert code to read the stream here.
+				String^ strfilename = openFileDialog1->InitialDirectory + openFileDialog1->FileName;
+				readFile = File::ReadAllText(strfilename);
+
+				richTextBox1->Text = readFile;
+
+				mystream->Close();
+			}
+		}
+	}
+
+	private: System::Void pictureToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		LPCTSTR input;
+		CString str;
+		int Width, Height;
+		long Size;
+
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			str = openFileDialog1->FileName;
+			input = (LPCTSTR)str;
+
+			//BMP Image Reading
+			buffer = LoadBMP(&Width, &Height, &Size, input);
+			raw_intensity = ConvertBMPToIntensity(buffer, Width, Height); // BMP Gray picture
+
+			pictureBox1->Width = Width;
+			pictureBox1->Height = Height;
+
+			//Display Gray Image into pictureBox2
+			Bitmap^ surface = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
+			pictureBox1->Image = surface;
+			Color c;
+
+			for (int row = 0; row < Height; row++)
+				for (int col = 0; col < Width; col++) {
+					c = Color::FromArgb(*(raw_intensity + row * Width + col), *(raw_intensity + row * Width + col), *(raw_intensity + row * Width + col));
+					surface->SetPixel(col, row, c);
+				}
+		}
+	}
+private: System::Void fer2013DSToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	Stream^ mystream;
+	OpenFileDialog^ openFileDialog1 = gcnew OpenFileDialog;
+
+	openFileDialog1->InitialDirectory = "";
+	openFileDialog1->Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+	openFileDialog1->FilterIndex = 2;
+	openFileDialog1->RestoreDirectory = true;
+
+	if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+	{
+		if ((mystream = openFileDialog1->OpenFile()) != nullptr)
+		{
+
+			// File pointer
+			fstream fin;
+
+			String^ fileName = openFileDialog1->FileName;
+			// Open an existing file
+			IntPtr ip = Marshal::StringToHGlobalAnsi(fileName);
+			const char* inputStr = static_cast<const char*>(ip.ToPointer());
+			std::string input(inputStr);
+
+			fin.open(inputStr, ios::in);
+
+			// Get the roll number
+			// of which the data is required
+			int rollnum, roll2, count = 0;
+			cout << "Enter the roll number "
+				<< "of the student to display details: ";
+			cin >> rollnum;
+
+			// Read the Data from the file
+			// as String Vector
+			vector<string> row;
+			string line, word, temp;
+			
+
+			while (fin >> temp) {
+
+				row.clear();
+
+				// read an entire row and
+				// store it in a string variable 'line'
+				std::getline(fin, line);
+
+				// read every column data of a row and
+				// store it in a string variable, 'word'
+				int k = 0;
+
+				for (int i = 0; i < line.length(); i++) {
+					if (line[i].Equals(' ')) {
+						word = line.substr(k , i-k);
+						row.push_back(word);
+
+						richTextBox1->Text += gcnew String(word.c_str()) + " ";
+						k = i;
+					}
+				}
+				richTextBox1->Text += "\n";
+			}
+
+
+
+			//readFile = File::ReadAllText(strfilename);
+			//richTextBox1->Text = readFile;
+
+			mystream->Close();
+		}
+	}
+}
+};
 }
